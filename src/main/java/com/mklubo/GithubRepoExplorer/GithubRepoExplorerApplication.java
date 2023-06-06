33073f18d6c0ce0344d5ba3eee910b43f395bc39
mklubowicz <michal.ghred@gmail.com>
2023-06-06 17:21:14 +0200

    init

---
+++ src/main/java/com/mklubo/GithubRepoExplorer/GithubRepoExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.mklubo.GithubRepoExplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepoExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mklubo/GithubRepoExplorer/GithubRepoExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mklubo.GithubRepoExplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepoExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
